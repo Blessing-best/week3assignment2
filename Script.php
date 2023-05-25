57f31730895578c206d718e5a18c33c4957ca52c
Blessing-best <133335635+Blessing-best@users.noreply.github.com>
2023-05-25 21:14:45 +0100

    Create Script.php

---
+++ Script.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Chukwu Blessing Ngozi";
+$regnumber = "2021/HND/33890/CS";
+$soo = "Ebonyi State";
+$topic = "Prison management system with Fingerprint verification (Biometric) Using Afikpo prison as a case study";
+
+
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic<br>";
+
+?>
